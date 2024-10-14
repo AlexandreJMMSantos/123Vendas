09c1474552fc76c8abbc03f7d96ca259aededd35
AlexandreJMMSantos <alexandretcq@gmail.com>
2024-10-13 22:05:38 -0300

    Adição de componentes EF

---
+++ 123Vendas/Controllers/VendasController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _123Vendas.Controllers
+{
+    public class VendasController : Controller
+    {
+        public VendasController()
+        {
+                
+        }
+    }
+}
